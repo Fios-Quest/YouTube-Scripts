8fecacc7c5c8dd4b54ebe54805fb9970470728fe
Gisleburt <Gisleburt@users.noreply.github.com>
2025-06-07 11:59:22 +0000

    deploy: 406924c0aba2e07946dcb3a205b5fda123122cf7

--- iriss/014-iterators/code/src/bin/into-iter.rs
+++ iriss/014-iterators/code/src/bin/into-iter.rs
@@ -1,5 +1,5 @@
 fn main() {
-    use std::collections::LinkedList;
+    // use std::collections::LinkedList;
 
     let hello = String::from("Hello");
     let world = String::from("World");
@@ -12,12 +12,14 @@ fn main() {
 
     assert_eq!(v, vec![String::from("Hello"), String::from("World")]);
 
+    use std::collections::LinkedList;
+
     let hello = String::from("Hello");
     let world = String::from("World");
 
     let mut ll = LinkedList::new();
-    ll.push_front(world);
-    ll.push_front(hello);
+    ll.push_back(hello);
+    ll.push_back(world);
 
     assert_eq!(
         ll.into_iter().collect::<Vec<_>>(),
