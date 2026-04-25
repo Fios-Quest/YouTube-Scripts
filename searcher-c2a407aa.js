7f583480b383b34359ad94664663dc72ebede1e5
Gisleburt <Gisleburt@users.noreply.github.com>
2026-04-25 15:33:05 +0000

    deploy: 6aaed5d402db884e1b1e71b0b90b2d995a8394ed

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-945b0ef0.js',
+                path_to_root + 'searchindex-6e68e63e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
